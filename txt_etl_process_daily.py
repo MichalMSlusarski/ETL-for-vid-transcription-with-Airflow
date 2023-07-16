556dcd8c5eccab92511364bf39ad356f0243e9e9
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2023-07-16 18:59:45 +0200

    Update txt_etl_process_daily.py

--- txt_etl_process_daily.py
+++ txt_etl_process_daily.py
@@ -4,8 +4,7 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.branch_operator import BranchPythonOperator
-
+from airflow.operators.python import BranchPythonOperator
 # functions:
 from get_data import get
 from transform_data import process
@@ -26,19 +25,19 @@ def get_data_func():
 
 def transform_data_func(**context):
     ti = context['ti']
-    data = ti.xcom_pull(task_ids='get_details')
+    data = ti.xcom_pull(task_ids='get_transcript')
     transformed_data = process(data)
     ti.xcom_push(key='transformed_data', value=transformed_data)
 
 def load_data_func(**context):
     ti = context['ti']
-    transformed_data = ti.xcom_pull(task_ids='transform_text_task')
+    transformed_data = ti.xcom_pull(task_ids='transform_text')
     loaded_data = load(transformed_data)
     return loaded_data
 
 def send_email_func(**context):
     ti = context['ti']
-    loaded_data = ti.xcom_pull(task_ids='load_text_task')
+    loaded_data = ti.xcom_pull(task_ids='load_data')
     email_body = f"The loaded data: {loaded_data}"
     
     email_notification = EmailOperator(
@@ -52,14 +51,14 @@ def send_email_func(**context):
     email_notification.execute(context)
 
 get_text_task = PythonOperator(
-    task_id="get_details",
+    task_id="get_transcript",
     python_callable=get_data_func,
     dag=dag,
 )
 
 def check_data_and_send_email(**context):
     ti = context['ti']
-    data = ti.xcom_pull(task_ids='get_details')
+    data = ti.xcom_pull(task_ids='get_transcript')
     if data is None:
         return 'send_email_notification'
     else:
@@ -73,14 +72,14 @@ check_data_task = BranchPythonOperator(
 )
 
 transform_text_task = PythonOperator(
-    task_id="transform_text_task",
+    task_id="transform_text",
     python_callable=transform_data_func,
     provide_context=True,
     dag=dag
 )
 
 load_text_task = PythonOperator(
-    task_id="load_text_task",
+    task_id="load_data",
     python_callable=load_data_func,
     provide_context=True,
     dag=dag
